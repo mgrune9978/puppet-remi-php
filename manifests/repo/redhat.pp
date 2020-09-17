159d6338a0e00a12d127f1c46b894ac28ffe94a7
Martin Grune <m.grune@imperial.ac.uk>
2020-09-17 09:11:20 +0100

    Updating remi to use regex to get short version

--- manifests/repo/redhat.pp
+++ manifests/repo/redhat.pp
@@ -6,27 +6,18 @@
 #   Class name of the repo under ::yum::repo
 #
 
-class php::repo::redhat (
-  $yum_repo = 'remi_php56',
-) {
+class php::repo::redhat {
+  # Get the version of php to install from global values
+  $version_real = $::php::globals::php_version
+
   # Convert the point version in to non point.  aka 5.6 -> 56
   $version_short = regsubst($version_real, '\.', '', 'G')
 
+  # Get os release name
   $releasever = $facts['os']['name'] ? {
     /(?i:Amazon)/ => '6',
     default       => '$releasever',  # Yum var
   }
-
-  if($::php::globals::php_version == undef) {
-    $version_real = '5.6'
-  } else {
-    $version_real = $::php::globals::php_version
-  }
-
-  if ($version_real == '5.5') {
-    fail('PHP 5.5 is no longer available for download')
-  }
-  assert_type(Pattern[/^\d\.\d/], $version_real)
 
   yumrepo { "remi-php${version_short}":
     ensure     => 'present',
